014d2eab03109497c2f88d07cd7fbaed1e64d7cb
wu <qq476705129@outlook.com>
2022-05-25 21:21:24 +0800

    完成后台首页

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -1,5 +1,18 @@
 ;(function(){
     $.ajaxPrefilter(function(option){
        option.url ='http://www.liulongbin.top:3007'+ option.url
+        //判断有/my的都加头部headers
+       if(option.url.indexOf('/my')!==-1){
+             option.headers={
+            Authorization:localStorage.getItem('token') || ''
+           }
+       }
+       //验证是否有token 没有就退出
+       option.complete =function(res){
+        if(res.responseJSON.status ===1){
+             localStorage.removeItem('token')
+              location.href = './login.html'
+        }
+     }
     })
 })()
